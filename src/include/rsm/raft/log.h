97d5d80e95dfe9363c9cf7e8c75082d4406940ae
gxj2003 <2061389208@qq.com>
2023-12-11 16:55:24 +0800

    finish lab3

--- src/include/rsm/raft/log.h
+++ src/include/rsm/raft/log.h
@@ -19,8 +19,10 @@ public:
 };
 class Entry{
 public:
+    int index;
     int term;
     int value;
+    int size;
 };
 /** 
  * RaftLog uses a BlockManager to manage the data..
@@ -32,11 +34,12 @@ public:
     ~RaftLog();
 
     /* Lab3: Your code here */
-    void restore(int &current_term, int &votedFor, std::vector<log_entry<Command>> &log);
+    void restore(int &current_term, int &votedFor, std::vector<log_entry<Command>> &log, std::vector<u8> &snapshot);
     void persist_meta(int currentTerm, int votedFor);
     void persist_log(std::vector<log_entry<Command>> log);
     void append_log(log_entry<Command>& entry);
     void append_logs(std::vector<log_entry<Command>>& log);
+    void persist_snapshot(std::vector<u8> &snapshot);
 
 
 private:
@@ -59,24 +62,35 @@ RaftLog<Command>::~RaftLog()
     /* Lab3: Your code here */
     bm_.reset();
 }
-
+// 0 if persist log
+// 1 currentTerm
+// 2 votedFor
+// 3 log_block_num
+// 4 log size
+// 5 if persist snapshot
+// 6 snapshot size
 /* Lab3: Your code here */
 template <typename Command>
-void RaftLog<Command>::restore(int &current_term, int &votedFor, std::vector<log_entry<Command>> &log) {
+void RaftLog<Command>::restore(int &current_term, int &votedFor, std::vector<log_entry<Command>> &log, std::vector<u8> &snapshot) {
     std::unique_lock<std::mutex> lock(mtx);
 
     std::vector<u8> meta_buffer(bm_->block_size());
     std::vector<u8> log_buffer(bm_->block_size());
+    std::vector<u8> snap_buffer(bm_->block_size());
     int entry_per_block = bm_->block_size() / sizeof(Entry);
-
+    const int snap_block_id = 17;
+//    snapshot.clear();
     bm_->read_block(0,meta_buffer.data());
     int *meta_p = reinterpret_cast<int *>(meta_buffer.data());
+
     if(meta_p[0] == 0){
         current_term = 0;
         votedFor = -1;
         log_entry<Command> first_empty_log = log_entry<Command>();
         log.push_back(first_empty_log);
 
+        meta_p[1] = 0;
+        meta_p[2] = -1;
         meta_p[3] = 1;
         meta_p[4] = 1;
         bm_->write_block(0,meta_buffer.data());
@@ -90,8 +104,8 @@ void RaftLog<Command>::restore(int &current_term, int &votedFor, std::vector<log
         int total_block_num = meta_p[3];
         int log_size = meta_p[4];
 
-        log_entry<Command> first_empty_log = log_entry<Command>();
-        log.push_back(first_empty_log);
+//        log_entry<Command> first_empty_log = log_entry<Command>();
+//        log.push_back(first_empty_log);
 
 
         for(block_id_t i = 0; i < total_block_num; i++){
@@ -99,20 +113,35 @@ void RaftLog<Command>::restore(int &current_term, int &votedFor, std::vector<log
             bm_->read_block(i + 1, log_buffer.data());
             for(int j = 0; j < entry_per_block; j++){
 
-                if(i * entry_per_block + j == 0){
-                    continue;
-                }
+//                if(i * entry_per_block + j == 0){
+//                    continue;
+//                }
                 if(i * entry_per_block + j >= log_size){
                     break;
                 }
                 auto *log_p = reinterpret_cast<Entry *>(log_buffer.data());
-                if(log_p[j].term == 0 && log_p[j].value == 0){
-                    break;
-                }
+//                if(log_p[j].term == 0 && log_p[j].value == 0){
+//                    break;
+//                }
                 Command cmd(log_p[j].value);
-                int index = log.size();
-                log_entry<Command> new_log(index,log_p[j].term,cmd);
+//                int index = log.size();
+                log_entry<Command> new_log(log_p[j].index,log_p[j].term,cmd);
                 log.push_back(new_log);
+            }
+        }
+
+        if(meta_p[5] == 1){
+            int snap_size = meta_p[6];
+            int snap_block_num = snap_size % bm_->block_size() == 0 ?
+                                 snap_size / bm_->block_size() : snap_size / bm_->block_size() + 1;
+            for(int i = 0; i < snap_block_num; i++){
+                bm_->read_block(i + snap_block_id, snap_buffer.data());
+                for(int j = 0; j < bm_->block_size(); j++){
+                    if(i * bm_->block_size() + j >= snap_size){
+                        break;
+                    }
+                    snapshot.push_back(snap_buffer[j]);
+                }
             }
         }
     }
@@ -139,24 +168,28 @@ void RaftLog<Command>::persist_log(std::vector<log_entry<Command>> log) {
 
 //    std::cerr<<"append "<<std::endl;
     int entry_per_block = bm_->block_size() / sizeof(Entry);
-    int total_block_num = log.size() % entry_per_block == 0 ?
+    int log_block_num = log.size() % entry_per_block == 0 ?
             log.size() / entry_per_block : log.size() / entry_per_block + 1;
 
     std::vector<u8> log_buffer(bm_->block_size());
-    for(block_id_t i = 0; i < total_block_num; i++){
+    for(block_id_t i = 0; i < log_block_num; i++){
 
         bm_->read_block(i + 1, log_buffer.data());
         for(int j = 0; j < entry_per_block; j++){
             if( i * entry_per_block + j >= log.size()){
                 break;
             }
-            if(i * entry_per_block + j == 0){
-
-                continue;
-            }
+//            if(i * entry_per_block + j == 0){
+//
+//                continue;
+//            }
 //            std::cerr<<"3"<<std::endl;
             auto *log_p = reinterpret_cast<Entry *>(log_buffer.data());
+            log_p[j].index = log[i * entry_per_block + j].index;
             log_p[j].term = log[i * entry_per_block + j].term;
+            if(i * entry_per_block + j == 0){
+                continue;
+            }
             log_p[j].value = log[i * entry_per_block + j].cmd.value;
 //            std::cerr<<log_p[j].term<<" "<<log_p[j].value<<std::endl;
 
@@ -169,7 +202,7 @@ void RaftLog<Command>::persist_log(std::vector<log_entry<Command>> log) {
     bm_->read_block(0, meta_buffer.data());
     int *meta_p = reinterpret_cast<int *>(meta_buffer.data());
     meta_p[0] = 1;
-    meta_p[3] = total_block_num;
+    meta_p[3] = log_block_num;
     meta_p[4] = log.size();
 
     bm_->write_block(0, meta_buffer.data());
@@ -188,21 +221,22 @@ void RaftLog<Command>::append_log(log_entry<Command> &entry) {
     bm_->read_block(0, meta_buffer.data());
     int *meta_p = reinterpret_cast<int *>(meta_buffer.data());
 
-    int total_block_num = meta_p[3];
+    int log_block_num = meta_p[3];
     int log_size = meta_p[4];
     block_id_t log_block_id;
     int block_entry_id;
 
     if(log_size % entry_per_block == 0){
-        log_block_id = total_block_num + 1;
+        log_block_id = log_block_num + 1;
         block_entry_id = 0;
     }
     else{
-        log_block_id = total_block_num;
+        log_block_id = log_block_num;
         block_entry_id = log_size % entry_per_block;
     }
     bm_->read_block(log_block_id, log_buffer.data());
     auto *log_p = reinterpret_cast<Entry *>(log_buffer.data());
+    log_p[block_entry_id].index = entry.index;
     log_p[block_entry_id].term = entry.term;
     log_p[block_entry_id].value = entry.cmd.value;
 
@@ -224,10 +258,43 @@ void RaftLog<Command>::append_logs(std::vector<log_entry<Command>>& log){
     }
 }
 
-// 0 if persist
+// 0 if persist log
 // 1 currentTerm
 // 2 votedFor
 // 3 total_block_num
 // 4 log size
+// 5 if persist snapshot
+// 6 snapshot size
 
+template <typename Command>
+void RaftLog<Command>::persist_snapshot(std::vector<u8> &snapshot){
+    std::unique_lock<std::mutex> lock(mtx);
+
+    const int snap_block_id = 17;
+    std::vector<u8> meta_buffer(bm_->block_size());
+    std::vector<u8> snap_buffer(bm_->block_size());
+    bm_->read_block(0, meta_buffer.data());
+    int *meta_p = reinterpret_cast<int *>(meta_buffer.data());
+
+    int snap_block_num = snapshot.size() % bm_->block_size() == 0 ?
+            snapshot.size() / bm_->block_size() : snapshot.size() / bm_->block_size() + 1;
+
+    for(int i = 0; i < snap_block_num; i++){
+        bm_->read_block(i + snap_block_id, snap_buffer.data());
+        for(int j = 0; j < bm_->block_size(); j++){
+            if(i * bm_->block_size() + j >= snapshot.size()){
+                break;
+            }
+            snap_buffer[j] = snapshot[i * bm_->block_size() + j];
+        }
+        bm_->write_block(i + snap_block_id, snap_buffer.data());
+        bm_->sync(i + snap_block_id);
+    }
+
+    meta_p[5] = 1;
+    meta_p[6] = (int)snapshot.size();
+    bm_->write_block(0, meta_buffer.data());
+    bm_->sync(0);
+
+}
 } /* namespace chfs */
